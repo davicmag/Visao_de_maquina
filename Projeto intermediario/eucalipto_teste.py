4dbed969cf99b4d20236f5eecf17df53bae40372
Rodrigo Piccini <rodrigopa5@al.insper.edu.br>
2026-04-22 17:45:52 -0300

    Remoção do vaso e limpeza da imagem

--- Projeto intermediario/eucalipto_teste.py
+++ Projeto intermediario/eucalipto_teste.py
@@ -20,22 +20,95 @@ plt.axis('off')
 
 # Gera mascara inicial (remove o fundo)
 mask_fundo = np.where((canal_b > 140), 0, 255).astype(np.uint8)
-plt.figure('Final')
+plt.figure('Sem Fundo')
 plt.imshow(mask_fundo, cmap='gray')
 plt.axis('off')
 
-# --- NOVO: máscara por posição (remove o vaso abaixo de 0.69 da altura) ---
-limite_base = int(0.624*h)
-mask_posicao = np.zeros((h, w), dtype=np.uint8)
-mask_posicao[:limite_base, :] = 255
 
-# Combina a máscara do fundo com a máscara por posição
-mask_final = cv2.bitwise_and(mask_fundo, mask_posicao)
+# Separando vaso da planta
 
-plt.figure('Mask Final (sem vaso)')
-plt.imshow(mask_final, cmap='gray')
+# kernel um pouco maior que a espessura do caule
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 3))
+
+# erosão para quebrar a ligação planta-vaso
+mask_erodida = cv2.erode(mask_fundo, kernel, iterations=1)
+
+# detectar componentes conectados na imagem erodida
+num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask_erodida, connectivity=8)
+
+# escolhe o componente mais baixo e com área suficiente
+melhor_id = -1
+melhor_score = -1
+
+for i in range(1, num_labels):
+
+    area = stats[i, cv2.CC_STAT_AREA]
+    x = stats[i, cv2.CC_STAT_LEFT]
+    y = stats[i, cv2.CC_STAT_TOP]
+    w = stats[i, cv2.CC_STAT_WIDTH]
+    h = stats[i, cv2.CC_STAT_HEIGHT]
+    cy = centroids[i][1]
+
+    if area < 300:
+
+        continue
+
+    # favorece o objeto mais abaixo na imagem
+    score = area + cy * 5
+
+    if score > melhor_score:
+
+        melhor_score = score
+        melhor_id = i
+
+
+# gera máscara só do vaso
+mascara_vaso = np.uint8(labels == melhor_id) * 255
+
+# fecha pequenos buracos pretos dentro do vaso
+kernel_fill = np.ones((9, 9), np.uint8)
+mascara_vaso = cv2.morphologyEx(mascara_vaso, cv2.MORPH_CLOSE, kernel_fill)
+
+# Aumenta um pouco a mascara para não ficar com "bordas" na hora de subtrair (maior e Y para não prejudicar o caule)
+kernel = np.ones((5, 13), np.uint8)
+mascara_vaso_ajustada = cv2.dilate(mascara_vaso, kernel, iterations=1)
+
+plt.figure("Mascara do vaso")
+plt.imshow(mascara_vaso_ajustada, cmap='gray')
 plt.axis('off')
 
+
+# Subtraindo vaso da imagem vaso+planta
+img_planta = cv2.subtract(mask_fundo, mascara_vaso_ajustada)
+
+
+# Remove pequenos ruídos da imagem da planta
+num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(img_planta, connectivity=8)
+
+img_limpa = np.zeros_like(img_planta)
+
+for i in range(1, num_labels):  
+    area = stats[i, cv2.CC_STAT_AREA]
+
+    if area > 90:  
+        img_limpa[labels == i] = 255
+
+plt.imshow(img_limpa, cmap='gray')
+plt.axis('off')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+#Bloob
 params = cv2.SimpleBlobDetector_Params()
 
 # Set blob color (0=black, 255=white)
@@ -66,10 +139,4 @@ params.filterByInertia = False
 detector = cv2.SimpleBlobDetector_create(params)
 
 
-
-
-
-
-
-plt.show()
 
